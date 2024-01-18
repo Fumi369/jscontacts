5966569287445a9a832b160f3209acb39f32dc5c
Fumi <>
2024-01-18 13:10:38 +0900

    initial push

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+const models = require("../models")
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  const now = new Date()
+  const contacts = await models.Contact.findAll()
+  res.render("index", { title: "連絡帳", now: now, contacts: contacts })
+})
+
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About" })
+})
+
+router.get("/contact_form", function (req, res, next) {
+  res.render("contact_form", { title: "連絡先フォーム" })
+})
+
+router.post("/contacts", async function (req, res, next) {
+  console.log("posted", req.body)
+  const num = new Date().getTime()
+  const contact = models.Contact.build({ name: req.body.name, email: req.body.email })
+  await contact.save()
+  res.redirect("/")
+})
+
+module.exports = router
