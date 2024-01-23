a6030dacbc451681499ebf75fac3880eca558a41
Fumi <>
2024-01-23 17:56:21 +0900

    Revert "11で加えた変更内容"
    This reverts commit 5652cb2465db3b86fa327c7053229f8e2543ad97.

--- sandbox/create_category.js
+++ sandbox/create_category.js
@@ -2,7 +2,7 @@ const models = require("../models")
 
 async function createCategory() {
   console.log("models.Category:", models.Category)
-  const category = models.Category.build({ name: "test1" })
+  const category = models.Category.build({ name: "test2" })
   await category.save()
 }
 
